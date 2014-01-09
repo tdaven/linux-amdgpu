420fe4be66d0ac1a4cc4c96fdc84a7f23d25a6d8
Alex Deucher <alexander.deucher@amd.com>
2014-01-09 10:59:56 -0500

    Revert "drm/radeon: disable CIK CP semaphores for now"
    This reverts commit 99b4f25122f43210278cde17a9d100906235a074.
    
    Semaphores work fine after further review and testing.
    Cc: 3.13 <stable@vger.kernel.org> # 3.13

--- drivers/gpu/drm/radeon/cik.c
+++ drivers/gpu/drm/radeon/cik.c
@@ -3564,8 +3564,6 @@ bool cik_semaphore_ring_emit(struct radeon_device *rdev,
 			     struct radeon_semaphore *semaphore,
 			     bool emit_wait)
 {
-/* TODO: figure out why semaphore cause lockups */
-#if 0
 	uint64_t addr = semaphore->gpu_addr;
 	unsigned sel = emit_wait ? PACKET3_SEM_SEL_WAIT : PACKET3_SEM_SEL_SIGNAL;
 
@@ -3574,9 +3572,6 @@ bool cik_semaphore_ring_emit(struct radeon_device *rdev,
 	radeon_ring_write(ring, (upper_32_bits(addr) & 0xffff) | sel);
 
 	return true;
-#else
-	return false;
-#endif
 }
 
 /**
