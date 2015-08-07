40171c529f7532df6c9452e0c02e2efb46f65147
Samuel Li <samuel.li@amd.com>
2015-08-07 11:54:02 -0400

    drm/amdgpu: add Stoney UVD support
    Signed-off-by: Samuel Li <samuel.li@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_uvd.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_uvd.c
@@ -52,6 +52,7 @@
 #endif
 #define FIRMWARE_TONGA		"amdgpu/tonga_uvd.bin"
 #define FIRMWARE_CARRIZO	"amdgpu/carrizo_uvd.bin"
+#define FIRMWARE_STONEY		"amdgpu/stoney_uvd.bin"
 
 /**
  * amdgpu_uvd_cs_ctx - Command submission parser context
@@ -81,6 +82,7 @@ MODULE_FIRMWARE(FIRMWARE_MULLINS);
 #endif
 MODULE_FIRMWARE(FIRMWARE_TONGA);
 MODULE_FIRMWARE(FIRMWARE_CARRIZO);
+MODULE_FIRMWARE(FIRMWARE_STONEY);
 
 static void amdgpu_uvd_note_usage(struct amdgpu_device *adev);
 static void amdgpu_uvd_idle_work_handler(struct work_struct *work);
@@ -118,6 +120,9 @@ int amdgpu_uvd_sw_init(struct amdgpu_device *adev)
 		break;
 	case CHIP_CARRIZO:
 		fw_name = FIRMWARE_CARRIZO;
+		break;
+	case CHIP_STONEY:
+		fw_name = FIRMWARE_STONEY;
 		break;
 	default:
 		return -EINVAL;
