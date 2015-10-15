aab76a32bbb6568c77f80ee0fceb305a4be64773
Samuel Li <Samuel.Li@amd.com>
2015-10-15 11:30:23 -0400

    drm/amdgpu: add support for VCE 3.x on stoney VCE on stoney is single pipe only.
    Signed-off-by: Samuel Li <Samuel.Li@amd.com>

--- drivers/gpu/drm/amd/amdgpu/vce_v3_0.c
+++ drivers/gpu/drm/amd/amdgpu/vce_v3_0.c
@@ -205,6 +205,11 @@ static unsigned vce_v3_0_get_harvest_config(struct amdgpu_device *adev)
 	u32 tmp;
 	unsigned ret;
 
+	if (adev->asic_type == CHIP_STONEY) {
+ 		ret = AMDGPU_VCE_HARVEST_VCE1;
+ 		return ret;
+ 	}
+
 	if (adev->flags & AMDGPU_IS_APU)
 		tmp = (RREG32_SMC(ixVCE_HARVEST_FUSE_MACRO__ADDRESS) &
 		       VCE_HARVEST_FUSE_MACRO__MASK) >>
