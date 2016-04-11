f828555714c6a8b6e9ccdc42d4a4fd51cb75f58e
Dave Airlie <airlied@redhat.com>
2016-04-11 17:37:17 +1000

    drm/amd/dal: avoid extra interfaces for scratch_critical_state.
    This is just an example of an over abstracted interface.
    
    Signed-off-by: Dave Airlie <airlied@redhat.com>
    Reviewed-by: Harry Wentland <harry.wentland@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/dal/dc/dce110/dce110_hw_sequencer.c
+++ drivers/gpu/drm/amd/dal/dc/dce110/dce110_hw_sequencer.c
@@ -34,6 +34,7 @@
 
 #include "gpu/dce110/dc_clock_gating_dce110.h"
 
+#include "bios/dce110/bios_dce110.h"
 #include "timing_generator.h"
 #include "mem_input.h"
 #include "opp.h"
@@ -514,12 +515,12 @@ static void program_fmt(
 	return;
 }
 
-static void update_bios_scratch_critical_state(struct adapter_service *as,
-		bool state)
+static void update_bios_scratch_critical_state(struct core_dc *dc,
+					       bool state)
 {
-	struct dc_bios *dcb = dal_adapter_service_get_bios_parser(as);
-
-	dc_bios_set_scratch_critical_state(dcb, state);
+#ifdef CONFIG_DRM_AMD_DAL_VBIOS_PRESENT
+	dce110_set_scratch_critical_state(dc->ctx, state);
+#endif
 }
 
 static void update_info_frame(struct pipe_ctx *pipe_ctx)
@@ -1206,8 +1207,7 @@ static enum dc_status apply_ctx_to_hw(
 		return DC_OK;
 
 	/* Apply new context */
-	update_bios_scratch_critical_state(context->res_ctx.pool.adapter_srv,
-			true);
+	update_bios_scratch_critical_state(dc, true);
 
 	for (i = 0; i < MAX_PIPES; i++) {
 		struct pipe_ctx *pipe_ctx = &context->res_ctx.pipe_ctx[i];
@@ -1275,8 +1275,7 @@ static enum dc_status apply_ctx_to_hw(
 
 	dc->hwss.set_displaymarks(dc, context);
 
-	update_bios_scratch_critical_state(context->res_ctx.pool.adapter_srv,
-			false);
+	update_bios_scratch_critical_state(dc, false);
 
 	switch_dp_clock_sources(dc, &context->res_ctx);
 
