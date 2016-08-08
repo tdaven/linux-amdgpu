b2b02ba67222519ae82358e1f57e9bca12ca9ec6
Emily Deng <Emily.Deng@amd.com>
2016-08-08 11:37:29 +0800

    drm/amdgpu: Define one variable for virtual display.
    For virtual display feature, define on variable in amdgpu.ko. When want to
    enable virtual display feature, need set the option "amdgpu.virtual_display=1".
    And then disable vga render and crtc if have DCE engine.
    
    Signed-off-by: Emily Deng <Emily.Deng@amd.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
@@ -90,6 +90,7 @@ unsigned amdgpu_pcie_gen_cap = 0;
 unsigned amdgpu_pcie_lane_cap = 0;
 unsigned amdgpu_cg_mask = 0xffffffff;
 unsigned amdgpu_pg_mask = 0xffffffff;
+int amdgpu_virtual_display = 0;
 
 MODULE_PARM_DESC(vramlimit, "Restrict VRAM for testing, in megabytes");
 module_param_named(vramlimit, amdgpu_vram_limit, int, 0600);
@@ -188,6 +189,9 @@ module_param_named(cg_mask, amdgpu_cg_mask, uint, 0444);
 
 MODULE_PARM_DESC(pg_mask, "Powergating flags mask (0 = disable power gating)");
 module_param_named(pg_mask, amdgpu_pg_mask, uint, 0444);
+
+MODULE_PARM_DESC(virtual_display, "enable virtual display (0 = disable virtual display)");
+module_param_named(virtual_display, amdgpu_virtual_display, int, 0444);
 
 static const struct pci_device_id pciidlist[] = {
 #ifdef CONFIG_DRM_AMDGPU_CIK
