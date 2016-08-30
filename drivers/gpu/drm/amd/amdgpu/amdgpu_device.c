21b2330b8327b20d77b6119ced9b78c653664c96
Chunming Zhou <David1.Zhou@amd.com>
2016-08-30 16:13:10 +0800

    drm/amdgpu: link all gtt when binding them V2
    V2:
    spin lock instead of mutex for gtt list
    
    Change-Id: I5dc3f9003e706d3a142aadb559bd85f6e0f2a03f
    Signed-off-by: Chunming Zhou <David1.Zhou@amd.com>
    Reviewed-by: Christian König <christian.koenig@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
@@ -1593,6 +1593,9 @@ int amdgpu_device_init(struct amdgpu_device *adev,
 	INIT_LIST_HEAD(&adev->shadow_list);
 	mutex_init(&adev->shadow_list_lock);
 
+	INIT_LIST_HEAD(&adev->gtt_list);
+	spin_lock_init(&adev->gtt_list_lock);
+
 	adev->rmmio_base = pci_resource_start(adev->pdev, 5);
 	adev->rmmio_size = pci_resource_len(adev->pdev, 5);
 	adev->rmmio = ioremap(adev->rmmio_base, adev->rmmio_size);
