298b9859a27660ce049b62563cff2beb3fe212c6
Roger.He <Hongbo.He@amd.com>
2017-01-09 15:01:45 +0800

    drm/amd/amdkcl: [4.8] fix dm_plane_helper_xxx_fb
    Change-Id: Ib45ecd0171dbc5dfe515d7af0dde25f451d69e59
    Signed-off-by: Roger.He <Hongbo.He@amd.com>
    Reviewed-by: Junwei Zhang <Jerry.Zhang@amd.com>
    
     Conflicts:
    	drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_types.c

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_types.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_types.c
@@ -1591,18 +1591,22 @@ static const struct drm_plane_funcs dm_plane_funcs = {
 	.atomic_destroy_state = dm_drm_plane_destroy_state,
 };
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 4, 0) && \
-	!defined(OS_NAME_RHEL_6) && \
-	!defined(OS_NAME_RHEL_7_3)
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 9, 0) || \
+	defined(OS_NAME_RHEL_6) || \
+	defined(OS_NAME_RHEL_7_3)
 static int dm_plane_helper_prepare_fb(
 	struct drm_plane *plane,
-	struct drm_framebuffer *fb,
+	struct drm_plane_state *new_state)
+#elif LINUX_VERSION_CODE >= KERNEL_VERSION(4, 4, 0)
+static int dm_plane_helper_prepare_fb(
+	struct drm_plane *plane,
 	const struct drm_plane_state *new_state)
 #else
 static int dm_plane_helper_prepare_fb(
 	struct drm_plane *plane,
-	struct drm_plane_state *new_state)
-#endif /* LINUX_VERSION_CODE < KERNEL_VERSION(4, 4, 0) */
+	struct drm_framebuffer *fb,
+	const struct drm_plane_state *new_state)
+#endif /* LINUX_VERSION_CODE < KERNEL_VERSION(4, 9, 0) */
 {
 	struct amdgpu_framebuffer *afb;
 	struct drm_gem_object *obj;
@@ -1646,19 +1650,22 @@ static int dm_plane_helper_prepare_fb(
 	}
 	return 0;
 }
-
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 4, 0) && \
-	!defined(OS_NAME_RHEL_6) && \
-	!defined(OS_NAME_RHEL_7_3)
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 9, 0) || \
+	defined(OS_NAME_RHEL_6) || \
+	defined(OS_NAME_RHEL_7_3)
 static void dm_plane_helper_cleanup_fb(
 	struct drm_plane *plane,
-	struct drm_framebuffer *fb,
+	struct drm_plane_state *old_state)
+#elif LINUX_VERSION_CODE >= KERNEL_VERSION(4, 4, 0)
+static void dm_plane_helper_cleanup_fb(
+	struct drm_plane *plane,
 	const struct drm_plane_state *old_state)
 #else
 static void dm_plane_helper_cleanup_fb(
 	struct drm_plane *plane,
-	struct drm_plane_state *old_state)
-#endif /* LINUX_VERSION_CODE < KERNEL_VERSION(4, 4, 0) */
+	struct drm_framebuffer *fb,
+	const struct drm_plane_state *old_state)
+#endif /* LINUX_VERSION_CODE < KERNEL_VERSION(4, 9, 0) */
 {
 	struct amdgpu_bo *rbo;
 	struct amdgpu_framebuffer *afb;
