961bc6dc959522be7a361a24220eeb2b91463532
Harry Wentland <harry.wentland@amd.com>
2017-03-03 14:50:00 -0500

    drm/amd/display: Add correct retain/release
    Needed by objs in dm_atomic_state
    
    Change-Id: I78ccd1fba5022dd686b5df290534b86fa089e30a
    Signed-off-by: Harry Wentland <harry.wentland@amd.com>
    Reviewed-by: Tony Cheng <Tony.Cheng@amd.com>
    Acked-by: Harry Wentland <Harry.Wentland@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_types.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_types.c
@@ -3157,11 +3157,15 @@ static uint32_t update_in_val_sets_stream(
 	}
 
 	val_sets[i].stream = new_stream;
+	dc_stream_retain(new_stream);
 	crtcs[i] = crtc;
 
 	if (i == set_count) {
 		/* nothing found. add new one to the end */
 		return set_count + 1;
+	} else {
+		/* update. relase old stream */
+		dc_stream_release(old_stream);
 	}
 
 	return set_count;
@@ -3183,6 +3187,7 @@ static uint32_t remove_from_val_sets(
 		return set_count;
 	}
 
+	dc_stream_release(stream);
 	set_count--;
 
 	for (; i < set_count; i++) {
@@ -3287,6 +3292,7 @@ int amdgpu_dm_atomic_check(struct drm_device *dev,
 		struct amdgpu_crtc *acrtc = to_amdgpu_crtc(crtc);
 
 		if (acrtc->stream) {
+			dc_stream_retain(acrtc->stream);
 			dm_state->set[dm_state->set_count].stream = acrtc->stream;
 			crtc_set[dm_state->set_count] = crtc;
 			++dm_state->set_count;
