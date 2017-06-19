e586cc381bf41c057882a998fd402bd8ed0dffab
Evan Quan <evan.quan@amd.com>
2017-06-19 16:45:33 +0800

    drm/amdgpu: use amdgpu to support CIK ASICs   - this patch is needed only by hybrid staging   - we may update packing script to add cik_support system config in future.     Then this commit will be not needed.
    Change-Id: Iba5a69fe1db01a290d8b27dff7116f9e534a74ae
    Signed-off-by: Evan Quan <evan.quan@amd.com>
    Reviewed-by: Junwei Zhang <Jerry.Zhang@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
@@ -265,8 +265,8 @@ module_param_named(si_support, amdgpu_si_support, int, 0444);
 #endif
 
 #ifdef CONFIG_DRM_AMDGPU_CIK
-int amdgpu_cik_support = 0;
-MODULE_PARM_DESC(cik_support, "CIK support (1 = enabled, 0 = disabled (default))");
+int amdgpu_cik_support = 1;
+MODULE_PARM_DESC(cik_support, "CIK support (1 = enabled (default), 0 = disabled)");
 module_param_named(cik_support, amdgpu_cik_support, int, 0444);
 #endif
 
