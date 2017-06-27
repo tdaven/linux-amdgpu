9bef59573e0836b083f2e20c59a1308c70c9ffc9
Harry Wentland <harry.wentland@amd.com>
2017-06-27 11:55:43 -0400

    drm/amd/display: Create dm_atomic_state
    We really want to use the new private_atomic_state but can't right now
    as we have to maintain some backward compatibility to older kernels. For
    now let's follow Intel's approach and extend the drm_atomic_state.
    
    Change-Id: I848bda7aa11d7731684e90e0d9e88191e8db8fb2
    Signed-off-by: Harry Wentland <harry.wentland@amd.com>
    Reviewed-by: Tony Cheng <Tony.Cheng@amd.com>
    Acked-by: Harry Wentland <Harry.Wentland@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_types.h
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_types.h
@@ -29,6 +29,7 @@
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 8, 0) || defined(OS_NAME_RHEL_7_4)
 
 #include <drm/drmP.h>
+#include <drm/drm_atomic.h>
 
 struct amdgpu_framebuffer;
 struct amdgpu_display_manager;
@@ -50,10 +51,11 @@ struct dm_crtc_state {
 
 #define to_dm_crtc_state(x)    container_of(x, struct dm_crtc_state, base)
 
-struct dm_plane_state {
-	struct drm_plane_state base;
-	struct dc_surface *dc_surface;
+struct dm_atomic_state {
+	struct drm_atomic_state base;
 };
+
+#define to_dm_atomic_state(x) container_of(x, struct dm_atomic_state, base)
 
 
 /*TODO Jodan Hersen use the one in amdgpu_dm*/
