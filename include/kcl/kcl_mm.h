949666b2446f88f31407267596696926f837e972
Roger.He <Hongbo.He@amd.com>
2017-07-06 13:00:02 +0800

    drm/amd/amdkcl: [RHEL 7.4] fix for get_user_pages
    Change-Id: Ida5e91139ebecf1e2d8beaec242e5a5186564fbe
    Reviewed-by: Evan Quan <evan.quan@amd.com>
    Signed-off-by: Roger.He <Hongbo.He@amd.com>

--- include/kcl/kcl_mm.h
+++ include/kcl/kcl_mm.h
@@ -8,11 +8,11 @@ static inline int kcl_get_user_pages(struct task_struct *tsk, struct mm_struct *
 				int write, int force, struct page **pages,
 				struct vm_area_struct **vmas)
 {
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 9, 0)
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 9, 0) || defined(OS_NAME_RHEL_7_4)
 	if (mm == current->mm)
 		return get_user_pages(start, nr_pages, write, pages, vmas);
 	else
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 10, 0)
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 10, 0) || defined(OS_NAME_RHEL_7_4)
 		return get_user_pages_remote(tsk, mm, start, nr_pages,
 				write, pages, vmas, NULL);
 #else
