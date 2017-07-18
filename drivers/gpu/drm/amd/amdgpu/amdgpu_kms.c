ROCm/ROCK-Kernel-Driver

drm/amdgpu: add query for aperture va range
Change-Id: I648c3784b13709fbcd79cfef4aa6dadeaadeee8c
Signed-off-by: Flora Cui <Flora.Cui@amd.com>
Reviewed-by: Jammy Zhou <Jammy.Zhou@amd.com>
Reviewed-by: Alex Deucher <alexander.deucher@amd.com>

Conflicts:
	drivers/gpu/drm/amd/amdgpu/amdgpu_kms.c
	include/uapi/drm/amdgpu_drm.h

drivers/gpu/drm/amd/amdgpu/amdgpu_kms.c
@@ -240,6 +240,23 @@ static int amdgpu_info_ioctl(struct drm_device *dev, void *data, struct drm_file
 		return -EINVAL;
 
 	switch (info->query) {
+	case AMDGPU_INFO_VIRTUAL_RANGE: {
+		struct drm_amdgpu_virtual_range range_info;
+		switch (info->virtual_range.aperture) {
+		case AMDGPU_SUA_APERTURE_PRIVATE:
+			range_info.start = adev->vm_manager.private_aperture_start;
+			range_info.end = adev->vm_manager.private_aperture_end;
+			break;
+		case AMDGPU_SUA_APERTURE_SHARED:
+			range_info.start = adev->vm_manager.shared_aperture_start;
+			range_info.end = adev->vm_manager.shared_aperture_end;
+			break;
+		default:
+			return -EINVAL;
+		}
+		return copy_to_user(out, &range_info,
+				min((size_t)size, sizeof(range_info))) ? -EFAULT : 0;
+	}
 	case AMDGPU_INFO_ACCEL_WORKING:
 		ui32 = adev->accel_working;
 		return copy_to_user(out, &ui32, min(size, 4u)) ? -EFAULT : 0;

tdaven/linux-amdgpu

drivers/gpu/drm/amd/amdgpu/amdgpu_kms.c
@@ -287,6 +287,23 @@ static int amdgpu_info_ioctl(struct drm_device *dev, void *data, struct drm_file
 		return -ENODEV;
 
 	switch (info->query) {
+	case AMDGPU_INFO_VIRTUAL_RANGE: {
+		struct drm_amdgpu_virtual_range range_info;
+		switch (info->virtual_range.aperture) {
+		case AMDGPU_SUA_APERTURE_PRIVATE:
+			range_info.start = adev->mc.private_aperture_start;
+			range_info.end = adev->mc.private_aperture_end;
+			break;
+		case AMDGPU_SUA_APERTURE_SHARED:
+			range_info.start = adev->mc.shared_aperture_start;
+			range_info.end = adev->mc.shared_aperture_end;
+			break;
+		default:
+			return -EINVAL;
+		}
+		return copy_to_user(out, &range_info,
+				min((size_t)size, sizeof(range_info))) ? -EFAULT : 0;
+	}
 	case AMDGPU_INFO_ACCEL_WORKING:
 		ui32 = adev->accel_working;
 		return copy_to_user(out, &ui32, min(size, 4u)) ? -EFAULT : 0;